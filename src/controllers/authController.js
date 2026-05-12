d5525c04d794d630a7c7f209159b920a74df8c83
RashedujjamanNoor <noorrk042@gmail.com>
2026-05-12 13:18:17 +0600

    create server, connect with database, register route,controller,usermodel

---
+++ src/controllers/authController.js
@@ -0,0 +1,33 @@
+const User = require("../models/User.js");
+const bcrypt = require("bcryptjs");
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exist",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { registerUser };
