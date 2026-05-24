6a11fd1893a902970eb8e520aadccb6c81a7bb78
RashedujjamanNoor <noorrk042@gmail.com>
2026-05-24 13:27:02 +0600

    create getMonthlyAnalytics controller funtion

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -43,6 +43,136 @@ const getAdminStats = async (req, res) => {
   }
 };
 
+// MONTHLY SALES ANALYTICS
+const getMonthlyAnalytics = async (req, res) => {
+  try {
+    // MONTHLY REVENUE
+    const salesData = await Order.aggregate([
+      {
+        $group: {
+          _id: {
+            month: { $month: "$createdAt" },
+          },
+
+          revenue: {
+            $sum: "$totalPrice",
+          },
+
+          orders: {
+            $sum: 1,
+          },
+        },
+      },
+
+      {
+        $sort: {
+          "_id.month": 1,
+        },
+      },
+    ]);
+
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+    const monthlyRevenue = salesData.map((item) => ({
+      month: months[item._id.month - 1],
+
+      revenue: item.revenue,
+
+      orders: item.orders,
+    }));
+
+    // CATEGORY ANALYTICS
+    const products = await Product.find();
+
+    const categoryMap = {};
+
+    products.forEach((product) => {
+      if (categoryMap[product.category]) {
+        categoryMap[product.category] += 1;
+      } else {
+        categoryMap[product.category] = 1;
+      }
+    });
+
+    const categoryAnalytics = Object.keys(categoryMap).map((key) => ({
+      name: key,
+
+      value: categoryMap[key],
+    }));
+
+    // ORDER STATUS ANALYTICS
+    const orders = await Order.find();
+
+    const statusMap = {};
+
+    orders.forEach((order) => {
+      if (statusMap[order.orderStatus]) {
+        statusMap[order.orderStatus] += 1;
+      } else {
+        statusMap[order.orderStatus] = 1;
+      }
+    });
+
+    const orderStatusAnalytics = Object.keys(statusMap).map((key) => ({
+      status: key,
+
+      count: statusMap[key],
+    }));
+
+    // USER GROWTH
+    const users = await User.find();
+
+    const userMap = {};
+
+    users.forEach((user) => {
+      const month = new Date(user.createdAt).getMonth();
+
+      const monthName = months[month];
+
+      if (userMap[monthName]) {
+        userMap[monthName] += 1;
+      } else {
+        userMap[monthName] = 1;
+      }
+    });
+
+    const userGrowthAnalytics = Object.keys(userMap).map((key) => ({
+      month: key,
+
+      users: userMap[key],
+    }));
+
+    res.status(200).json({
+      success: true,
+
+      analytics: {
+        monthlyRevenue,
+        categoryAnalytics,
+        orderStatusAnalytics,
+        userGrowthAnalytics,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   getAdminStats,
+  getMonthlyAnalytics,
 };
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -4,8 +4,12 @@ const router = express.Router();
 const protect = require("../middleware/authMiddleware.js");
 const adminMiddleware = require("../middleware/adminMiddleware.js");
 
-const { getAdminStats } = require("../controllers/adminController.js");
+const {
+  getAdminStats,
+  getMonthlyAnalytics,
+} = require("../controllers/adminController.js");
 
 router.get("/dashboard", protect, adminMiddleware, getAdminStats);
+router.get("/analytics", protect, adminMiddleware, getMonthlyAnalytics);
 
 module.exports = router;
